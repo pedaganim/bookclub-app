be75e9f2ee04eec1bd4afbc27b6c803ae96c9e77
pedaganim <madhukar.pedagani@gmail.com>
2025-09-06 14:41:04 +1000

    tf: guard outputs when manage_dns=false to avoid count index errors

--- bookclub-app/backend/terraform/outputs.tf
+++ bookclub-app/backend/terraform/outputs.tf
@@ -5,10 +5,10 @@ output "api_domain_name" {
 
 output "api_domain_cloudfront" {
   description = "API Gateway domain CloudFront distribution DNS name"
-  value       = try(aws_api_gateway_domain_name.api_domain.cloudfront_domain_name, null)
+  value       = var.manage_dns ? aws_api_gateway_domain_name.api_domain[0].cloudfront_domain_name : null
 }
 
 output "api_domain_cf_zone_id" {
   description = "API Gateway domain CloudFront hosted zone id"
-  value       = try(aws_api_gateway_domain_name.api_domain.cloudfront_zone_id, null)
+  value       = var.manage_dns ? aws_api_gateway_domain_name.api_domain[0].cloudfront_zone_id : null
 }
