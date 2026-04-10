bab06c15ae7f68536c8b01060b77f35f6b42f3a5
pedaganim <madhukar.pedagani@gmail.com>
2026-04-10 18:54:54 +1000

    Revert "Switch to Cognito Custom Resources to support adoption of existing UserPool and UserPoolDomain"
    This reverts commit 03f18bad309d16d3b518d913e9939cda4eae1051.

--- bookclub-app/backend/src/custom-resources/cognito-resource-manager.js
+++ bookclub-app/backend/src/custom-resources/cognito-resource-manager.js
@@ -136,22 +136,19 @@ exports.handler = async (event, context) => {
   console.log('Event:', JSON.stringify(event, null, 2));
   
   const { RequestType, ResourceProperties } = event;
-  const { Action } = ResourceProperties;
+  const { Action, UserPoolId, ProviderName, ProviderType, ProviderDetails, AttributeMapping } = ResourceProperties;
 
   try {
-    let result = {};
-    let physicalResourceId = event.PhysicalResourceId || context.logStreamName;
+    let result;
+    const physicalResourceId = `${UserPoolId}-${ProviderName}`;
     
     switch (RequestType) {
       case 'Create':
-      case 'Update':
         if (Action === 'CreateOrUpdateIdentityProvider') {
-          const { UserPoolId, ProviderName, ProviderType, ProviderDetails, AttributeMapping } = ResourceProperties;
-          physicalResourceId = `${UserPoolId}-${ProviderName}`;
           const exists = await identityProviderExists(UserPoolId, ProviderName);
           if (exists) {
             console.log(`Identity Provider ${ProviderName} already exists, updating it`);
-            await updateIdentityProvider({
+            result = await updateIdentityProvider({
               UserPoolId,
               ProviderName,
               ProviderType,
@@ -159,7 +156,7 @@ exports.handler = async (event, context) => {
               AttributeMapping
             });
           } else {
-            await createIdentityProvider({
+            result = await createIdentityProvider({
               UserPoolId,
               ProviderName,
               ProviderType,
@@ -167,100 +164,32 @@ exports.handler = async (event, context) => {
               AttributeMapping
             });
           }
-          result = { ProviderName };
-        } else if (Action === 'EnsureUserPool') {
-          const { UserPoolName, Policies, Schema, AutoVerifiedAttributes } = ResourceProperties;
-          physicalResourceId = UserPoolName;
-          
-          // List user pools to find by name
-          const listRes = await cognitoIdp.listUserPools({ MaxResults: 60 }).promise();
-          const existingPool = (listRes.UserPools || []).find(p => p.Name === UserPoolName);
-          
-          if (existingPool) {
-            console.log(`User Pool ${UserPoolName} already exists, adopting it: ${existingPool.Id}`);
-            physicalResourceId = existingPool.Id;
-            result = { UserPoolId: existingPool.Id, Arn: `arn:aws:cognito-idp:${process.env.AWS_REGION}:${context.invokedFunctionArn.split(':')[4]}:userpool/${existingPool.Id}` };
-          } else {
-            console.log(`Creating new User Pool: ${UserPoolName}`);
-            const createRes = await cognitoIdp.createUserPool({
-              PoolName: UserPoolName,
-              Policies,
-              Schema,
-              AutoVerifiedAttributes
-            }).promise();
-            physicalResourceId = createRes.UserPool.Id;
-            result = { UserPoolId: createRes.UserPool.Id, Arn: createRes.UserPool.Arn };
-          }
-        } else if (Action === 'EnsureUserPoolClient') {
-          const { UserPoolId, ClientName, ExplicitAuthFlows, PreventUserExistenceErrors, SupportedIdentityProviders, AllowedOAuthFlows, AllowedOAuthScopes, CallbackURLs, LogoutURLs, AllowedOAuthFlowsUserPoolClient } = ResourceProperties;
-          
-          // List clients for the pool
-          const listRes = await cognitoIdp.listUserPoolClients({ UserPoolId, MaxResults: 60 }).promise();
-          const existingClient = (listRes.UserPoolClients || []).find(c => c.ClientName === ClientName);
-          
-          const clientParams = {
+        } else {
+          throw new Error(`Unknown action: ${Action}`);
+        }
+        break;
+
+      case 'Update':
+        if (Action === 'CreateOrUpdateIdentityProvider') {
+          result = await updateIdentityProvider({
             UserPoolId,
-            ClientName,
-            ExplicitAuthFlows,
-            PreventUserExistenceErrors,
-            SupportedIdentityProviders,
-            AllowedOAuthFlows,
-            AllowedOAuthScopes,
-            CallbackURLs,
-            LogoutURLs,
-            AllowedOAuthFlowsUserPoolClient
-          };
-
-          if (existingClient) {
-            console.log(`User Pool Client ${ClientName} already exists, adopting and updating it: ${existingClient.ClientId}`);
-            physicalResourceId = existingClient.ClientId;
-            await cognitoIdp.updateUserPoolClient({
-              ...clientParams,
-              ClientId: existingClient.ClientId
-            }).promise();
-            result = { ClientId: existingClient.ClientId };
-          } else {
-            console.log(`Creating new User Pool Client: ${ClientName}`);
-            const createRes = await cognitoIdp.createUserPoolClient(clientParams).promise();
-            physicalResourceId = createRes.UserPoolClient.ClientId;
-            result = { ClientId: createRes.UserPoolClient.ClientId };
-          }
-        } else if (Action === 'EnsureUserPoolDomain') {
-          const { UserPoolId, Domain } = ResourceProperties;
-          physicalResourceId = Domain;
-
-          try {
-            const descRes = await cognitoIdp.describeUserPoolDomain({ Domain }).promise();
-            if (descRes.DomainDescription && descRes.DomainDescription.UserPoolId === UserPoolId) {
-              console.log(`Domain ${Domain} already exists for this User Pool, adopting it.`);
-            } else if (descRes.DomainDescription) {
-              console.log(`Domain ${Domain} exists but for a DIFFERENT User Pool (${descRes.DomainDescription.UserPoolId}). Deleting and recreating...`);
-              await cognitoIdp.deleteUserPoolDomain({ Domain, UserPoolId: descRes.DomainDescription.UserPoolId }).promise();
-              await cognitoIdp.createUserPoolDomain({ Domain, UserPoolId }).promise();
-            }
-          } catch (e) {
-            if (e.code === 'ResourceNotFoundException') {
-              console.log(`Creating new User Pool Domain: ${Domain}`);
-              await cognitoIdp.createUserPoolDomain({ Domain, UserPoolId }).promise();
-            } else {
-              throw e;
-            }
-          }
-          result = { Domain };
+            ProviderName,
+            ProviderType,
+            ProviderDetails,
+            AttributeMapping
+          });
         } else {
           throw new Error(`Unknown action: ${Action}`);
         }
         break;
 
       case 'Delete':
-        if (ResourceProperties.DeletionPolicy === 'Retain') {
-          console.log(`DeletionPolicy is Retain, keeping resource`);
-        } else if (Action === 'CreateOrUpdateIdentityProvider') {
-          await deleteIdentityProvider(ResourceProperties.UserPoolId, ResourceProperties.ProviderName);
-        } else if (Action === 'EnsureUserPoolDomain') {
-          await cognitoIdp.deleteUserPoolDomain({ Domain: ResourceProperties.Domain, UserPoolId: ResourceProperties.UserPoolId }).promise();
+        // Only delete if DeletionPolicy is not Retain
+        if (ResourceProperties.DeletionPolicy !== 'Retain') {
+          await deleteIdentityProvider(UserPoolId, ProviderName);
+        } else {
+          console.log(`DeletionPolicy is Retain, keeping Identity Provider: ${ProviderName}`);
         }
-        // We typically don't auto-delete UserPools or Clients in this manager for safety
         result = {};
         break;
 
@@ -268,7 +197,7 @@ exports.handler = async (event, context) => {
         throw new Error(`Unknown request type: ${RequestType}`);
     }
 
-    await sendResponse(event, context, 'SUCCESS', result, physicalResourceId);
+    await sendResponse(event, context, 'SUCCESS', { ProviderName }, physicalResourceId);
   } catch (error) {
     console.error('Error:', error);
     await sendResponse(event, context, 'FAILED', { Error: error.message });
